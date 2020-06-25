487207260b7566fd7f6d920f33227f0cfb0fb458
Joker <495669631@qq.com>
2020-06-26 00:15:51 +0800

    exchange popup fix

---
+++ assets/scripts/Game/ui/main/ExchangePopUp.js
@@ -0,0 +1,22 @@
+let jkr = require("Jkr");
+
+cc.Class({
+    extends: jkr.BasePage,
+
+    properties: {
+        woodNumLabel: cc.Label
+    },
+
+    showPageBegan: function() {
+
+    },
+
+    onClickConfirm: function() {
+
+    },
+
+    onClickCancel: function() {
+
+    },
+
+});
